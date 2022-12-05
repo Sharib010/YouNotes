79b30831103bd07329d0e829b52378c78a5ea3f5
Sharib010 <sharibrizvi01@example.com>
2022-12-05 21:12:45 +0530

    YourNote
    v1.1.0

--- index.js
+++ index.js
@@ -3,27 +3,25 @@ const app = express();
 const dotenv = require("dotenv");
 const cors = require("cors");
 const path = require("path");
-const cookieParser = require('cookie-parser');
+const cookieParser = require("cookie-parser");
 // const { constants } = require('buffer');
 
-app.use(cookieParser())
-dotenv.config({ path: './config.env' });
+app.use(cookieParser());
+dotenv.config({ path: "./config.env" });
 const PORT = process.env.PORT || 4000;
 
-require('./DB/conn');
+require("./DB/conn");
 // app.use(express.urlencoded());
 app.use(express.json());
-app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 
-app.use(require('./router/auth'));
+app.use(require("./router/auth"));
 
-
-app.use(express.static(path.join(__dirname, './client/build')))
+app.use(express.static(path.join(__dirname, "./client/build")));
 
 app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, './client/build/index.html'));
-})
-
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on PORT ${PORT}`);
--- router/auth.js
+++ router/auth.js
@@ -1,143 +1,141 @@
 const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcrypt");
-const jwt = require('jsonwebtoken');
-var cookieParser = require('cookie-parser');
-const authenticate = require('../Middleware/Authentication')
+const jwt = require("jsonwebtoken");
+var cookieParser = require("cookie-parser");
+const authenticate = require("../Middleware/Authentication");
 
 require("../DB/conn");
 const schema = require("../Model/userSchema");
 
 const User = schema.User;
-const blackNotes = schema.blackNotes
+const blackNotes = schema.blackNotes;
 
 // -------------------For login and Register---------------------
 
 router.post("/api/login", async (req, res) => {
-    const { email, password } = req.body;
-    const userLogin = await User.findOne({ email: email });
-    if (userLogin) {
-        const isMatch = await bcrypt.compare(password, userLogin.password);
+  const { email, password } = req.body;
+  const userLogin = await User.findOne({ email: email });
+  if (userLogin) {
+    const isMatch = await bcrypt.compare(password, userLogin.password);
 
-        token = await userLogin.generateAuthToken();
-        // res.cookie("jwtoken", token);
+    token = await userLogin.generateAuthToken();
+    // res.cookie("jwtoken", token);
 
-        if (!isMatch) {
-            res.status(400).json({ error: "invalid crediential password" });
-        } else {
-            // res.json({ message: "user sigin successfully" });
-            res.send(token)
-        }
+    if (!isMatch) {
+      res.status(400).json({ error: "invalid crediential password" });
     } else {
-        res.status(400).json({ error: "invalid crediential email" });
+      // res.json({ message: "user sigin successfully" });
+      res.send(token);
     }
+  } else {
+    res.status(400).json({ error: "invalid crediential email" });
+  }
 });
 
+router.post("/api/register", async (req, res) => {
+  const { name, email, password, cpassword } = req.body;
+  if (!name || !email || !password) {
+    return res.status(422).json({ error: "fill all" });
+  }
 
-
-router.post('/api/register', async (req, res) => {
-    const { name, email, password, cpassword } = req.body;
-    if (!name || !email || !password || !cpassword) {
-        return res.status(422).json({ error: "fill all" });
+  try {
+    const userExist = await User.findOne({ email: email });
+    if (userExist) {
+      return res.status(422).json({ error: "Email aleady exist" });
+    } else if (password != cpassword) {
+      return res.status(422).json({ error: "paswword not matching" });
+    } else {
+      const user = new User({ name, email, password, cpassword });
+      const userRegister = await user.save();
+      if (userRegister) {
+        res.status(201).json({ message: "data save successfully" });
+      }
     }
-
-    try {
-        const userExist = await User.findOne({ email: email });
-        if (userExist) {
-            return res.status(422).json({ error: "Email aleady exist" });
-        } else if (password != cpassword) {
-            return res.status(422).json({ error: "paswword not matching" });
-        } else {
-            const user = new User({ name, email, password, cpassword });
-            const userRegister = await user.save();
-            if (userRegister) {
-                res.status(201).json({ message: "Register successfully" });
-            }
-        }
-    } catch (err) {
-        console.log(err);
-    }
+  } catch (err) {
+    console.log(err);
+  }
 });
-
 
 // ------------------------------------------Notes Database----------------------------------------
 
 router.post("/api/createNote", async (req, res) => {
-    const { title, note, date, day, note_Key, color, fontStyle, fontWeight, fontSize } = req.body;
-    const blackNotesObj = new blackNotes({
-        title,
-        note,
-        date,
-        day,
-        note_Key,
-        color,
-        fontStyle,
-        fontWeight,
-        fontSize
-    });
-    const saveNote = await blackNotesObj.save()
+  const { title, note, date, day, note_Key, color, fontStyle, fontWeight, fontSize } = req.body;
+  const blackNotesObj = new blackNotes({
+    title,
+    note,
+    date,
+    day,
+    note_Key,
+    color,
+    fontStyle,
+    fontWeight,
+    fontSize
+  });
+  if (!title || !note || !note_Key) {
+    return res.status(422).json({ error: "fill all" });
+  }
+  else {
+    const saveNote = await blackNotesObj.save();
     if (!saveNote) {
-        console.log("error occured");
+      console.log("error occured");
+    } else {
+      res.send(saveNote);
     }
-    else {
-        res.send(saveNote);
-    }
+  }
+
 });
 
-
 router.post("/api/getNotes", (req, res) => {
-    const { email } = req.body
+  const { email } = req.body;
 
-    blackNotes.find({ note_Key: email }, (err, inotesList) => {
-        if (err) {
-            console.log(err);
-        } else {
-            res.status(200).send(inotesList);
-            // console.log(inotesList)
-        }
-    });
-
+  blackNotes.find({ note_Key: email }, (err, inotesList) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.status(200).send(inotesList);
+      // console.log(inotesList)
+    }
+  });
 });
 
 router.post("/api/deleteNotes", (req, res) => {
-    const { id, email } = req.body
-    blackNotes.deleteOne({ _id: id }, () => {
-        blackNotes.find({ note_Key: email }, (err, data) => {
-            if (err) {
-                console.log(err);
-            } else {
-                res.status(200).send(data);
-            }
-        });
+  const { id, email } = req.body;
+  blackNotes.deleteOne({ _id: id }, () => {
+    blackNotes.find({ note_Key: email }, (err, data) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.status(200).send(data);
+      }
     });
+  });
 });
 
 router.post("/api/editNote", (req, res) => {
-    const { title, note, date, day, id } = req.body;
-    const editNote = {
-        title: title,
-        note: note,
-        day: day,
-        date: date
+  const { title, note, date, day, id } = req.body;
+  const editNote = {
+    title: title,
+    note: note,
+    day: day,
+    date: date,
+  };
+  blackNotes.findOneAndUpdate({ _id: id }, editNote, (err) => {
+    if (err) {
+      console.log("error occured");
+    } else {
+      res.status(200);
     }
-    blackNotes.findOneAndUpdate({ _id: id }, editNote, (err) => {
-        if (err) {
-            console.log("error occured");
-        }
-        else {
-            res.status(200);
-        }
-    });
+  });
 });
 
-router.get('/api/getdata', authenticate, (req, res) => {
-    res.send(req.rootUser)
-})
+router.get("/api/getdata", authenticate, (req, res) => {
+  res.send(req.rootUser);
+});
 
-router.get('/getCookie', (req, res) => {
-    console.log(req.cookies)
-    res.send(req.cookies.jwtoken)
-})
-
+router.get("/getCookie", (req, res) => {
+  console.log(req.cookies);
+  res.send(req.cookies.jwtoken);
+});
 
 module.exports = router;
